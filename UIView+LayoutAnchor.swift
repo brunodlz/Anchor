ee430dc892e71ba5e87fcd854e2d5d70184a2b47
Kai Lee <klee@spotsetter.com>
2016-04-04 14:26:12 -0700

    Update tab to 4 spaces

--- UIView+LayoutAnchor.swift
+++ UIView+LayoutAnchor.swift
@@ -23,102 +23,102 @@
 import UIKit
 
 extension UIView {
-  
-  // MARK: superview
-  func anchorTopToSuperview() -> NSLayoutConstraint {
-    return anchorTopTo((superview?.topAnchor)!)
-  }
-  
-  func anchorBottomToSuperview() -> NSLayoutConstraint {
-    return anchorBottomTo((superview?.bottomAnchor)!)
-  }
-  
-  func anchorLeftToSuperview() -> NSLayoutConstraint {
-    return anchorLeftTo((superview?.leftAnchor)!)
-  }
-  
-  func anchorRightToSuperview() -> NSLayoutConstraint {
-    return anchorRightTo((superview?.rightAnchor)!)
-  }
-  
-  func anchorLeadingToSuperview() -> NSLayoutConstraint {
-    return anchorLeadingTo((superview?.leadingAnchor)!)
-  }
-  
-  func anchorTrailingToSuperview() -> NSLayoutConstraint {
-    return anchorTrailingTo((superview?.trailingAnchor)!)
-  }
-  
-  func anchorCenterXToSuperview() -> NSLayoutConstraint {
-    return anchor(self.centerXAnchor, toAnchor: (superview?.centerXAnchor)!)
-  }
-  
-  func anchorCenterYToSuperview() -> NSLayoutConstraint {
-    return anchor(self.centerYAnchor, toAnchor: (superview?.centerYAnchor)!)
-  }
-  
-  func anchorCenterToSuperview() {
-    anchorCenterXToSuperview()
-    anchorCenterYToSuperview()
-  }
-  
-  func anchorEdgesToSuperView() {
-    anchorTopToSuperview()
-    anchorBottomToSuperview()
-    anchorLeftToSuperview()
-    anchorRightToSuperview()
-  }
-  
-  func anchorEdgesToSuperView(omit omit: NSLayoutAttribute) {
-    var anchors = [anchorTopToSuperview,
-      anchorBottomToSuperview,
-      anchorLeftToSuperview,
-      anchorRightToSuperview]
-    switch excluding {
-    case .Top: _ = anchors.removeAtIndex(0)
-    case .Bottom: _ = anchors.removeAtIndex(1)
-    case .Leading, .Left: _ = anchors.removeAtIndex(2)
-    case .Trailing, .Right: _ = anchors.removeAtIndex(3)
-    default: break
+    
+    // MARK: superview
+    func anchorTopToSuperview() -> NSLayoutConstraint {
+        return anchorTopTo((superview?.topAnchor)!)
     }
     
-    for anchor in anchors {
-      anchor()
+    func anchorBottomToSuperview() -> NSLayoutConstraint {
+        return anchorBottomTo((superview?.bottomAnchor)!)
     }
-  }
-  
-  // MARK: anchorTo NSLayoutAnchor
-  func anchorTopTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
-    return anchor(self.topAnchor, toAnchor: toAnchor, offset: offset)
-  }
-  
-  func anchorBottomTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
-    return anchor(self.bottomAnchor, toAnchor: toAnchor)
-  }
-  
-  func anchorLeftTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
-    return anchor(self.leftAnchor, toAnchor: toAnchor, offset: offset)
-  }
-  
-  func anchorRightTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
-    return anchor(self.rightAnchor, toAnchor: toAnchor, offset: offset)
-  }
-  
-  func anchorLeadingTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
-    return anchor(self.leadingAnchor, toAnchor: toAnchor)
-  }
-  
-  func anchorTrailingTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
-    return anchor(self.trailingAnchor, toAnchor: toAnchor)
-  }
-  
-  // MARK: private
-  private func anchor(anchor: NSLayoutAnchor,
-    toAnchor: NSLayoutAnchor,
-    offset: CGFloat = 0) -> NSLayoutConstraint {
-      self.translatesAutoresizingMaskIntoConstraints = false
-      let constraint = anchor.constraintEqualToAnchor(toAnchor, constant: offset)
-      constraint.active = true
-      return constraint
-  }
+    
+    func anchorLeftToSuperview() -> NSLayoutConstraint {
+        return anchorLeftTo((superview?.leftAnchor)!)
+    }
+    
+    func anchorRightToSuperview() -> NSLayoutConstraint {
+        return anchorRightTo((superview?.rightAnchor)!)
+    }
+    
+    func anchorLeadingToSuperview() -> NSLayoutConstraint {
+        return anchorLeadingTo((superview?.leadingAnchor)!)
+    }
+    
+    func anchorTrailingToSuperview() -> NSLayoutConstraint {
+        return anchorTrailingTo((superview?.trailingAnchor)!)
+    }
+    
+    func anchorCenterXToSuperview() -> NSLayoutConstraint {
+        return anchor(self.centerXAnchor, toAnchor: (superview?.centerXAnchor)!)
+    }
+    
+    func anchorCenterYToSuperview() -> NSLayoutConstraint {
+        return anchor(self.centerYAnchor, toAnchor: (superview?.centerYAnchor)!)
+    }
+    
+    func anchorCenterToSuperview() {
+        anchorCenterXToSuperview()
+        anchorCenterYToSuperview()
+    }
+    
+    func anchorEdgesToSuperView() {
+        anchorTopToSuperview()
+        anchorBottomToSuperview()
+        anchorLeftToSuperview()
+        anchorRightToSuperview()
+    }
+    
+    func anchorEdgesToSuperView(omit omit: NSLayoutAttribute) {
+        var anchors = [anchorTopToSuperview,
+                       anchorBottomToSuperview,
+                       anchorLeftToSuperview,
+                       anchorRightToSuperview]
+        switch excluding {
+        case .Top: _ = anchors.removeAtIndex(0)
+        case .Bottom: _ = anchors.removeAtIndex(1)
+        case .Leading, .Left: _ = anchors.removeAtIndex(2)
+        case .Trailing, .Right: _ = anchors.removeAtIndex(3)
+        default: break
+        }
+        
+        for anchor in anchors {
+            anchor()
+        }
+    }
+    
+    // MARK: anchorTo NSLayoutAnchor
+    func anchorTopTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
+        return anchor(self.topAnchor, toAnchor: toAnchor, offset: offset)
+    }
+    
+    func anchorBottomTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
+        return anchor(self.bottomAnchor, toAnchor: toAnchor)
+    }
+    
+    func anchorLeftTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
+        return anchor(self.leftAnchor, toAnchor: toAnchor, offset: offset)
+    }
+    
+    func anchorRightTo(toAnchor: NSLayoutAnchor, offset: CGFloat = 0) -> NSLayoutConstraint {
+        return anchor(self.rightAnchor, toAnchor: toAnchor, offset: offset)
+    }
+    
+    func anchorLeadingTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
+        return anchor(self.leadingAnchor, toAnchor: toAnchor)
+    }
+    
+    func anchorTrailingTo(toAnchor: NSLayoutAnchor) -> NSLayoutConstraint {
+        return anchor(self.trailingAnchor, toAnchor: toAnchor)
+    }
+    
+    // MARK: private
+    private func anchor(anchor: NSLayoutAnchor,
+                        toAnchor: NSLayoutAnchor,
+                        offset: CGFloat = 0) -> NSLayoutConstraint {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let constraint = anchor.constraintEqualToAnchor(toAnchor, constant: offset)
+        constraint.active = true
+        return constraint
+    }
 }
