5b540ae895d065ed5e8512c4e0adaf66e4aaac9e
Kai Lee <klee@spotsetter.com>
2016-04-06 15:32:56 -0700

    Add anchor to edge tests

--- AnchorTests/AnchorTests.swift
+++ AnchorTests/AnchorTests.swift
@@ -18,6 +18,88 @@ class AnchorTests: XCTestCase {
         expect(self.view.anchor().view).to(beIdenticalTo(view))
     }
     
+    func testChainingAndActivatingAnchor() {
+        expect(self.view.translatesAutoresizingMaskIntoConstraints).to(beTrue())
+        let anchor = view.anchor()
+            .topToSuperview()
+            .leftToSuperview()
+            .activate()
+        
+        expect(self.view.translatesAutoresizingMaskIntoConstraints).to(beFalse())
+        expect(anchor.top?.active).to(beTrue())
+        expect(anchor.left?.active).to(beTrue())
+    }
+    
+    func testEdgeAnchors() {
+        let anchor = view.anchor()
+            .top(to: window.top)
+            .left(to: window.left, constant: 1)
+            .bottom(to: window.bottom, constant: -2)
+            .right(to: window.right, constant: -3)
+        
+        expect(anchor.top?.firstItem).to(beIdenticalTo(view))
+        expect(anchor.top?.firstAttribute).to(equal(NSLayoutAttribute.Top))
+        expect(anchor.top?.secondItem).to(beIdenticalTo(window))
+        expect(anchor.top?.secondAttribute).to(equal(NSLayoutAttribute.Top))
+        expect(anchor.top?.relation).to(equal(NSLayoutRelation.Equal))
+        expect(anchor.top?.constant).to(equal(0))
+        
+        expect(anchor.left?.firstItem).to(beIdenticalTo(view))
+        expect(anchor.left?.firstAttribute).to(equal(NSLayoutAttribute.Left))
+        expect(anchor.left?.secondItem).to(beIdenticalTo(window))
+        expect(anchor.left?.secondAttribute).to(equal(NSLayoutAttribute.Left))
+        expect(anchor.left?.relation).to(equal(NSLayoutRelation.Equal))
+        expect(anchor.left?.constant).to(equal(1))
+        
+        expect(anchor.bottom?.firstItem).to(beIdenticalTo(view))
+        expect(anchor.bottom?.firstAttribute).to(equal(NSLayoutAttribute.Bottom))
+        expect(anchor.bottom?.secondItem).to(beIdenticalTo(window))
+        expect(anchor.bottom?.secondAttribute).to(equal(NSLayoutAttribute.Bottom))
+        expect(anchor.bottom?.relation).to(equal(NSLayoutRelation.Equal))
+        expect(anchor.bottom?.constant).to(equal(-2))
+        
+        expect(anchor.right?.firstItem).to(beIdenticalTo(view))
+        expect(anchor.right?.firstAttribute).to(equal(NSLayoutAttribute.Right))
+        expect(anchor.right?.secondItem).to(beIdenticalTo(window))
+        expect(anchor.right?.secondAttribute).to(equal(NSLayoutAttribute.Right))
+        expect(anchor.right?.relation).to(equal(NSLayoutRelation.Equal))
+        expect(anchor.right?.constant).to(equal(-3))
+    }
+    
+    func testGreaterOrEqualAnchor() {
+        let anchor = view.anchor()
+            .top(greaterOrEqual: window.top)
+            .left(greaterOrEqual: window.left, constant: 1)
+            .bottom(greaterOrEqual: window.bottom, constant: -2)
+            .right(greaterOrEqual: window.right, constant: -3)
+        
+        expect(anchor.top?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
+        expect(anchor.top?.constant).to(equal(0))
+        expect(anchor.left?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
+        expect(anchor.left?.constant).to(equal(1))
+        expect(anchor.bottom?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
+        expect(anchor.bottom?.constant).to(equal(-2))
+        expect(anchor.right?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
+        expect(anchor.right?.constant).to(equal(-3))
+    }
+    
+    func testLessOrEqualAnchor() {
+        let anchor = view.anchor()
+            .top(lesserOrEqual: window.top)
+            .left(lesserOrEqual: window.left, constant: 1)
+            .bottom(lesserOrEqual: window.bottom, constant: -2)
+            .right(lesserOrEqual: window.right, constant: -3)
+        
+        expect(anchor.top?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
+        expect(anchor.top?.constant).to(equal(0))
+        expect(anchor.left?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
+        expect(anchor.left?.constant).to(equal(1))
+        expect(anchor.bottom?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
+        expect(anchor.bottom?.constant).to(equal(-2))
+        expect(anchor.right?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
+        expect(anchor.right?.constant).to(equal(-3))
+    }
+    
     func testSuperviewAnchorsWithOutSuperview() {
         view.removeFromSuperview()
         
@@ -34,70 +116,25 @@ class AnchorTests: XCTestCase {
             .bottomToSuperview(constant: -2)
             .rightToSuperview(constant: -3)
         
-        expect(anchor.top?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.top?.firstAttribute).to(equal(NSLayoutAttribute.Top))
-        expect(anchor.top?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.top?.secondAttribute).to(equal(NSLayoutAttribute.Top))
-        expect(anchor.top?.constant).to(equal(0))
-        expect(anchor.top?.active).to(beFalse())
-        
-        expect(anchor.left?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.left?.firstAttribute).to(equal(NSLayoutAttribute.Left))
-        expect(anchor.left?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.left?.secondAttribute).to(equal(NSLayoutAttribute.Left))
-        expect(anchor.left?.constant).to(equal(1))
-        expect(anchor.left?.active).to(beFalse())
-        
-        expect(anchor.bottom?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.bottom?.firstAttribute).to(equal(NSLayoutAttribute.Bottom))
-        expect(anchor.bottom?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.bottom?.secondAttribute).to(equal(NSLayoutAttribute.Bottom))
-        expect(anchor.bottom?.constant).to(equal(-2))
-        expect(anchor.bottom?.active).to(beFalse())
-        
-        expect(anchor.right?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.right?.firstAttribute).to(equal(NSLayoutAttribute.Right))
-        expect(anchor.right?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.right?.secondAttribute).to(equal(NSLayoutAttribute.Right))
-        expect(anchor.right?.constant).to(equal(-3))
-        expect(anchor.right?.active).to(beFalse())
+        expect(anchor.top).toNot(beNil())
+        expect(anchor.left).toNot(beNil())
+        expect(anchor.bottom).toNot(beNil())
+        expect(anchor.right).toNot(beNil())
     }
     
     func testSuperviewEdgesAnchor() {
         let anchor = view.anchor().edgesToSuperview()
         
-        expect(anchor.top?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.top?.firstAttribute).to(equal(NSLayoutAttribute.Top))
-        expect(anchor.top?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.top?.secondAttribute).to(equal(NSLayoutAttribute.Top))
-        expect(anchor.top?.constant).to(equal(0))
-        expect(anchor.top?.active).to(beFalse())
-        
-        expect(anchor.left?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.left?.firstAttribute).to(equal(NSLayoutAttribute.Left))
-        expect(anchor.left?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.left?.secondAttribute).to(equal(NSLayoutAttribute.Left))
-        expect(anchor.left?.constant).to(equal(0))
-        expect(anchor.left?.active).to(beFalse())
-        
-        expect(anchor.bottom?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.bottom?.firstAttribute).to(equal(NSLayoutAttribute.Bottom))
-        expect(anchor.bottom?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.bottom?.secondAttribute).to(equal(NSLayoutAttribute.Bottom))
-        expect(anchor.bottom?.constant).to(equal(0))
-        expect(anchor.bottom?.active).to(beFalse())
-        
-        expect(anchor.right?.firstItem).to(beIdenticalTo(view))
-        expect(anchor.right?.firstAttribute).to(equal(NSLayoutAttribute.Right))
-        expect(anchor.right?.secondItem).to(beIdenticalTo(window))
-        expect(anchor.right?.secondAttribute).to(equal(NSLayoutAttribute.Right))
-        expect(anchor.right?.constant).to(equal(0))
-        expect(anchor.right?.active).to(beFalse())
+        expect(anchor.top).toNot(beNil())
+        expect(anchor.left).toNot(beNil())
+        expect(anchor.bottom).toNot(beNil())
+        expect(anchor.right).toNot(beNil())
     }
     
     func testSuperviewEdgesAnchorWithInsets() {
         let anchor = view.anchor()
             .edgesToSuperview(insets: UIEdgeInsets(top: 0, left: 1, bottom: -2, right: -3))
+        
         expect(anchor.top?.constant).to(equal(0))
         expect(anchor.left?.constant).to(equal(1))
         expect(anchor.bottom?.constant).to(equal(-2))
@@ -106,47 +143,10 @@ class AnchorTests: XCTestCase {
     
     func testSuperviewEdgesAnchorWithOmittedEdge() {
         let anchor = view.anchor().edgesToSuperview(omitEdge: .Bottom)
+        
         expect(anchor.top).toNot(beNil())
         expect(anchor.left).toNot(beNil())
         expect(anchor.bottom).to(beNil())
         expect(anchor.right).toNot(beNil())
-    }
-    
-    func testActivatingAnchor() {
-        expect(self.view.translatesAutoresizingMaskIntoConstraints).to(beTrue())
-        let anchor = view.anchor()
-            .topToSuperview()
-            .leftToSuperview()
-            .activate()
-        
-        expect(self.view.translatesAutoresizingMaskIntoConstraints).to(beFalse())
-        expect(anchor.top?.active).to(beTrue())
-        expect(anchor.left?.active).to(beTrue())
-    }
-    
-    func testGreaterOrEqualAnchor() {
-        let anchor = view.anchor()
-            .top(greaterOrEqual: window.top)
-            .left(greaterOrEqual: window.left)
-            .bottom(greaterOrEqual: window.bottom)
-            .right(greaterOrEqual: window.right)
-        
-        expect(anchor.top?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
-        expect(anchor.right?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
-        expect(anchor.bottom?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
-        expect(anchor.right?.relation).to(equal(NSLayoutRelation.GreaterThanOrEqual))
-    }
-    
-    func testLessOrEqualAnchor() {
-        let anchor = view.anchor()
-            .top(lesserOrEqual: window.top)
-            .left(lesserOrEqual: window.left)
-            .bottom(lesserOrEqual: window.bottom)
-            .right(lesserOrEqual: window.right)
-        
-        expect(anchor.top?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
-        expect(anchor.right?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
-        expect(anchor.bottom?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
-        expect(anchor.right?.relation).to(equal(NSLayoutRelation.LessThanOrEqual))
     }
 }
